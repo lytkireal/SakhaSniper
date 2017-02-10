405c627670c8f34bb64fa520dfcb2e9b894b98da
Artem Lytkin <lytkinreal@mail.ru>
2017-02-10 15:07:21 +0300

    Step № 9
    Put the app in landscape orientation

---
+++ SquirrelsEye/AboutViewController.swift
@@ -0,0 +1,42 @@
+//
+//  AboutViewController.swift
+//  SquirrelsEye
+//
+//  Created by macbook air on 10.02.17.
+//  Copyright © 2017 macbook air. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+  
+  
+  @IBAction func close() {
+  
+    dismiss(animated: true, completion: nil)
+  
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+    }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
